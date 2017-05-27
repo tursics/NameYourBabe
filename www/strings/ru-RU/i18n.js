1e5f5ba1a07d3105ba9d4210dfbf706f60695f72
Thomas Tursics <thomas@tursics.de>
2017-05-27 23:51:34 +0200

    added a Russian language

---
+++ www/strings/ru-RU/i18n.js
@@ -0,0 +1,68 @@
+if (typeof gDict === "undefined") { gDict = []; }
+
+gDict.ru = {
+	"appLang": 'ru-RU',
+	"appName": 'Назовите своего малыша',
+	"appDescription": '##Никогда не было так легко найти идеально подходящее имя для вашего малыша! Приложение показывает имя, популярное в вашем регионе. Имя не понравилось? Просто встряхните смартфон, и вы получите новое имя.<br><br>НЕТ НИЧЕГО ПРОЩЕ!<br><br>Und das bekommst du:<br>- die beliebtesten Namen in Österreich<br>- über 200 beliebteste Namen für Mädchen<br>- über 175 beliebteste Namen für Jungen<br>- über 3700 Namen insgesamt<br>- große Vielfalt aus über 10 Jahren<br>- Lieblingsnamen markieren',
+
+	"homeSub": 'Никогда не было проще назвать своего ребенка',
+	"homeGirl": 'Девочка',
+	"homeBoy": 'Мальчик',
+	"homeBoyGirl": '##Jungen und Mädchen',
+	"homeFav": 'Избранные',
+	"homeSearch": 'Поиск имен',
+	"homeSearchShort": 'Поиск',
+	"homeShare": 'Поделиться',
+	"homeImprint": 'Выходные данные',
+	"homeBack": 'Назад',
+	"homeHome": 'Главная',
+
+	"canvasFilter": '##Zeige nur',
+	"canvasCopyright": '##Copyright',
+	"canvasHelp": '##Anleitung',
+	"canvasSettings": '##Einstellungen',
+
+	"resultGirl": 'Это будет девочка',
+	"resultBoy": 'Это будет мальчик',
+	"resultShake": 'Потрясите для отображения следующего имени',
+	"resultButtonShake": 'Потрясти',
+	"resultButtonNext": '##Nächster Name',
+
+	"detailAlternate": 'Похожие имена',
+	"detailSpread": 'Распространение',
+	"detailMore": 'Читать дальше',
+	"detailGenericBoy": '%name% это мужское имя.',
+	"detailGenericGirl": '%name% это женское имя.',
+	"detailGenericBoth": '##%name% ist sowohl ein männlicher als auch weiblicher Name.',
+	"detailChartPos": '##Platz %pos% in %nuts% %year%',
+	"detailChartPosShort": '##Platz %pos%',
+	"detailFav": 'Добавить в избранные',
+
+	"imprintCopyright": 'Авторские права 2013-2017, Thomas Tursics',
+	"imprintWebHome": '##Webseite',
+	"imprintWebSupport": '##Support',
+	"imprintTitleData": "Используемые статистические данные",
+	"imprintTitleImage": "Использованные изображения",
+	"imprintTitleText": "Используемые тексты",
+	"imprintTitleWikipedia": "Википедия",
+	"imprintTitleRateMe": "##Bitte bewerte die App",
+	"imprintBodyWikipedia": "Информация об именах была взята из Википедии. Ссылка на связанную веб-страницу может быть найдена под описанием имени.",
+	"imprintCopyrightOther": "##Zusätzliche Quellen",
+	"imprintCopyrightThanks": "##Vielen Dank an",
+
+	"settingsOn": 'An',
+	"settingsOff": 'Aus',
+	"settingsNewName": 'Zufälliger Name durch',
+	"settingsShake": 'Schütteln',
+	"settingsSwipe": 'Wischen',
+	"settingsLanguage": 'Benutze Sprache',
+	"settingsAutomatic": 'Automatisch',
+	"settingsDone": 'Fertig',
+
+	"filterTitle": 'Filter',
+	"filterText": 'Wende den Filter auf alle Funktionen an, wie z.B. Namensvorschlag, Suche, ähnliche Namen und geografische Verbreitung.',
+	"filterWorld": 'Weltweit',
+
+	"favoritesEmpty": 'Du hast noch keine Lieblingsnamen hinzugefügt. Schüttle und wische jetzt Namen, und markiere die, die dir am besten gefallen.',
+	"searchTooMany": 'Wow - mehr als 100 Namen wurden gefunden!<br>Bitte schränke deine Suche ein ...'
+};
